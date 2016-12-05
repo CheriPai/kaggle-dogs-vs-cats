77bbdc9350515159fab3f751d2c9ccf534d55400
dat <datdo@rocketmail.com>
2016-12-04 17:58:54 -0800

    Separate train & val. Save model

--- train.py
+++ train.py
@@ -3,9 +3,9 @@ from keras.preprocessing import image
 from keras.models import Model
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras import backend as K
-from utils import preprocess
+from utils import get_gen
 
-nb_epoch = 5
+nb_epoch = 100
 
 base_model = InceptionV3(weights="imagenet", include_top=False)
 
@@ -15,7 +15,8 @@ x = Dense(1024, activation="relu")(x)
 predictions = Dense(2, activation="softmax")(x)
 
 model = Model(input=base_model.input, output=predictions)
-model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy", metrics=["accuracy", "msle"])
+model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy", metrics=["accuracy", "binary_crossentropy"])
 
-gen = preprocess()
-model.fit_generator(gen, nb_epoch=nb_epoch, verbose=1, samples_per_epoch=23000, nb_val_samples=2000)
+train_gen, val_gen = get_gen()
+model.fit_generator(train_gen, nb_epoch=nb_epoch, verbose=1, samples_per_epoch=20000, validation_data=val_gen, nb_val_samples=5000)
+model.save("data/model.h5")
--- utils.py
+++ utils.py
@@ -1,16 +1,22 @@
 from keras.preprocessing.image import ImageDataGenerator
 
 
-def preprocess(mode="train"):
+def get_gen(mode="train"):
     if mode == "train":
         datagen = ImageDataGenerator(
             rescale=1./255,
             horizontal_flip=True)
-        gen = datagen.flow_from_directory(
+        train_gen = datagen.flow_from_directory(
             "data/train",
             target_size=(128, 128),
             batch_size=128,
             class_mode="binary")
+        val_gen = datagen.flow_from_directory(
+            "data/val",
+            target_size=(128, 128),
+            batch_size=128,
+            class_mode="binary")
+        return train_gen, val_gen
     else:
         datagen = ImageDataGenerator(
             rescale=1./255)
@@ -19,4 +25,4 @@ def preprocess(mode="train"):
             target_size=(128, 128),
             batch_size=128,
             class_mode="binary")
-    return gen
+        return gen
