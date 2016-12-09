847da7ef41c8ba1915480b77affb55b2c940dcbc
dat <datdo@rocketmail.com>
2016-12-09 09:16:08 -0800

    Save to data/output. Remove mode

--- utils.py
+++ utils.py
@@ -2,18 +2,17 @@ from keras.preprocessing.image import ImageDataGenerator
 
 
 def get_gen():
-    if mode == "train":
-        datagen = ImageDataGenerator(
-            rescale=1./255,
-            horizontal_flip=True)
-        train_gen = datagen.flow_from_directory(
-            "data/train",
-            target_size=(128, 128),
-            batch_size=128,
-            class_mode="binary")
-        val_gen = datagen.flow_from_directory(
-            "data/val",
-            target_size=(128, 128),
-            batch_size=128,
-            class_mode="binary")
-        return train_gen, val_gen
+    datagen = ImageDataGenerator(
+        rescale=1./255,
+        horizontal_flip=True)
+    train_gen = datagen.flow_from_directory(
+        "data/train",
+        target_size=(128, 128),
+        batch_size=128,
+        class_mode="binary")
+    val_gen = datagen.flow_from_directory(
+        "data/val",
+        target_size=(128, 128),
+        batch_size=128,
+        class_mode="binary")
+    return train_gen, val_gen
