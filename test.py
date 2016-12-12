f875f63d38b073901ba0bbe25b8b388c02cf8ced
dat <datdo@rocketmail.com>
2016-12-12 12:05:41 -0800

    Place image in array based on image id

--- test.py
+++ test.py
@@ -16,9 +16,13 @@ model.load_weights("data/model.h5")
 model.compile(optimizer="rmsprop", loss="binary_crossentropy", metrics=["accuracy"])
 
 imgs = np.zeros((12500, 150, 150, 3))
-for i, f in enumerate(os.listdir(test_dir)):
+for f in os.listdir(test_dir):
     img_path = os.path.join(test_dir, f)
+    img_id = int(img_path.split("/")[-1].split(".")[0])
     img = load_img(img_path, target_size=((150,150)))
-    imgs[i] = img_to_array(img) / 255
+    imgs[img_id-1] = img_to_array(img) / 255
 
-predictions = model.predict(imgs, batch_size=256)
+df = pd.DataFrame()
+df["id"] = np.arange(1, 12501)
+df["label"] = model.predict(imgs, batch_size=256)
+df.to_csv("data/output", index=False)
