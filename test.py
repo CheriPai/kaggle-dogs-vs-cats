13316f1bb71132bf329adc1b2bfc9e713d68bf9c
dat <datdo@rocketmail.com>
2016-12-13 10:19:37 -0800

    Save weights with loss in title

--- test.py
+++ test.py
@@ -7,6 +7,8 @@ from utils import get_gen
 
 
 test_dir = "data/test"
+num_test = 12500
+img_size = 150
 
 
 with open("data/model.json", "r") as json_file:
@@ -15,14 +17,14 @@ model = model_from_json(json_model)
 model.load_weights("data/model.h5")
 model.compile(optimizer="rmsprop", loss="binary_crossentropy", metrics=["accuracy"])
 
-imgs = np.zeros((12500, 150, 150, 3))
+imgs = np.zeros((num_test, img_size, img_size, 3))
 for f in os.listdir(test_dir):
     img_path = os.path.join(test_dir, f)
     img_id = int(img_path.split("/")[-1].split(".")[0])
-    img = load_img(img_path, target_size=((150,150)))
+    img = load_img(img_path, target_size=((img_size,img_size)))
     imgs[img_id-1] = img_to_array(img) / 255
 
 df = pd.DataFrame()
-df["id"] = np.arange(1, 12501)
+df["id"] = np.arange(1, num_test+1)
 df["label"] = model.predict(imgs, batch_size=256)
 df.to_csv("data/output", index=False)
--- train.py
+++ train.py
@@ -13,12 +13,13 @@ model.add(base_model)
 model.add(GlobalAveragePooling2D(input_shape=base_model.output_shape[1:]))
 model.add(Dense(1024, activation="relu"))
 model.add(Dense(1, activation="sigmoid"))
-model.compile(optimizer="rmsprop", loss="binary_crossentropy", metrics=["accuracy"])
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
 
 train_gen, val_gen = get_gen()
-model.fit_generator(train_gen, nb_epoch=nb_epoch, verbose=1, samples_per_epoch=20000, validation_data=val_gen, nb_val_samples=5000)
+history = model.fit_generator(train_gen, nb_epoch=nb_epoch, verbose=1, samples_per_epoch=20000, validation_data=val_gen, nb_val_samples=5000)
+val_loss = round(history.history["val_loss"][-1], 4)
 
 model_json = model.to_json()
 with open("data/model.json", "w") as json_file:
     json_file.write(model_json)
-model.save("data/model.h5")
+model.save("data/{}.h5".format(val_loss))
