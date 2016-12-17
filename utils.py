d714009a4e2ba825b8e129d8d68de19bfee41c6e
dat <datdo@rocketmail.com>
2016-12-17 10:40:42 -0800

    Use height and width shifting for data augmentation

--- utils.py
+++ utils.py
@@ -5,8 +5,9 @@ img_size = 150
 def get_gen():
     train_datagen = ImageDataGenerator(
         rescale=1./255,
-        shear_range=0.2,
         zoom_range=0.2,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
         horizontal_flip=True)
     test_datagen = ImageDataGenerator(rescale=1./255)
     train_gen = train_datagen.flow_from_directory(
