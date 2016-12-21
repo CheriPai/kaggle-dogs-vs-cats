17522dcba28193882bc218b819d65f18e0292156
dat <datdo@rocketmail.com>
2016-12-20 16:44:50 -0800

    Add early stopper

--- train.py
+++ train.py
@@ -1,12 +1,12 @@
 from keras.applications.inception_v3 import InceptionV3
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.layers import Dense, GlobalAveragePooling2D, Dropout
 from keras.models import Sequential
 from keras.optimizers import RMSprop
 from keras import backend as K
 from utils import get_gen
 
-nb_epoch = 100
+nb_epoch = 500
 
 base_model = InceptionV3(weights="imagenet", include_top=False)
 for layer in base_model.layers:
@@ -27,8 +27,9 @@ for layer in model.layers:
     layer.trainable = True
 
 model.compile(optimizer=RMSprop(lr=0.001), loss="binary_crossentropy", metrics=["accuracy"])
+earlystopper = EarlyStopping(monitor="val_loss", patience=20, mode="min")
 checkpointer = ModelCheckpoint(filepath="data/weights.hdf5", monitor="val_loss", verbose=1, save_best_only=True, mode="min")
-history = model.fit_generator(train_gen, nb_epoch=nb_epoch, verbose=1, samples_per_epoch=20000, validation_data=val_gen, nb_val_samples=5000, callbacks=[checkpointer])
+history = model.fit_generator(train_gen, nb_epoch=nb_epoch, verbose=1, samples_per_epoch=20000, validation_data=val_gen, nb_val_samples=5000, callbacks=[checkpointer, earlystopper])
 
 model_json = model.to_json()
 with open("data/model.json", "w") as json_file:
