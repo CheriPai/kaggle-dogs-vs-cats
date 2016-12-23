22226a7f3fd504266e7fc068e05dd2459164289d
dat <datdo@rocketmail.com>
2016-12-22 23:14:05 -0800

    Finetine VGG16

--- train.py
+++ train.py
@@ -1,35 +1,28 @@
-from keras.applications.inception_v3 import InceptionV3
+from keras.applications.vgg16 import VGG16
 from keras.callbacks import EarlyStopping, ModelCheckpoint
-from keras.layers import Dense, GlobalAveragePooling2D, Dropout
+from keras.layers import Dense, GlobalAveragePooling2D, Dropout, Flatten
 from keras.models import Sequential
-from keras.optimizers import RMSprop
+from keras.optimizers import Adam
 from keras import backend as K
 from utils import get_gen
 
-nb_epoch = 500
+nb_epoch = 100
 
-base_model = InceptionV3(weights="imagenet", include_top=False)
+base_model = VGG16(weights="imagenet", include_top=False)
 for layer in base_model.layers:
     layer.trainable = False
 
 model = Sequential()
 model.add(base_model)
 model.add(GlobalAveragePooling2D(input_shape=base_model.output_shape[1:]))
-model.add(Dense(1024, activation="relu"))
-model.add(Dropout(0.5))
 model.add(Dense(1, activation="sigmoid"))
-model.compile(optimizer="rmsprop", loss="binary_crossentropy", metrics=["accuracy"])
+model.compile(optimizer=Adam(lr=0.001), loss="binary_crossentropy", metrics=["accuracy"])
 
 train_gen, val_gen = get_gen()
-model.fit_generator(train_gen, nb_epoch=3, verbose=1, samples_per_epoch=20000)
 
-for layer in model.layers:
-    layer.trainable = True
-
-model.compile(optimizer=RMSprop(lr=0.001), loss="binary_crossentropy", metrics=["accuracy"])
 earlystopper = EarlyStopping(monitor="val_loss", patience=20, mode="min")
 checkpointer = ModelCheckpoint(filepath="data/weights.hdf5", monitor="val_loss", verbose=1, save_best_only=True, mode="min")
-history = model.fit_generator(train_gen, nb_epoch=nb_epoch, verbose=1, samples_per_epoch=20000, validation_data=val_gen, nb_val_samples=5000, callbacks=[checkpointer, earlystopper])
+history = model.fit_generator(train_gen, nb_epoch=nb_epoch, verbose=1, samples_per_epoch=train_gen.nb_sample, validation_data=val_gen, nb_val_samples=val_gen.nb_sample, callbacks=[checkpointer, earlystopper])
 
 model_json = model.to_json()
 with open("data/model.json", "w") as json_file:
